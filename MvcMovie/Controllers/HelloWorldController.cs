210f129d036f7fa71b2927ef387463c6894d5c95
staahl2468 <staahl2468@gmail.com>
2023-01-08 21:35:40 +0100

    Fjernet ubrugte links

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,20 +8,6 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public IActionResult Welcome(string name, int numTimes = 1)
-        {
-            ViewData["Message"] = "Hello " + name;
-            ViewData["NumTimes"] = numTimes;
-
-            return View();
-        }
+    
     }
 }
